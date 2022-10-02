14e3f51362505d300db2b31ad61de81aa14ac4b2
Edgar <Edgar@MacBook-Pro.fritz.box>
2022-10-02 17:53:09 +0200

    refactoring recycler adapter

--- app/src/main/java/com/dosparta/triviagame/AnswersRecyclerAdapter.kt
+++ app/src/main/java/com/dosparta/triviagame/AnswersRecyclerAdapter.kt
@@ -6,15 +6,10 @@ import android.view.ViewGroup
 import android.widget.Button
 import androidx.recyclerview.widget.RecyclerView
 
+private const val HIGHLIGHT_CORRECT_ANSWER = "HIGHLIGHT_CORRECT_ANSWER"
+
 class AnswersRecyclerAdapter(private val answers: List<Answer>, private val listener: OnCorrectAnswerListener) :
     RecyclerView.Adapter<AnswersRecyclerAdapter.ViewHolder>() {
-    class ViewHolder (view: View) : RecyclerView.ViewHolder(view) {
-        val questionButton: Button
-
-        init {
-            questionButton = view.findViewById(R.id.button_question)
-        }
-    }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.question_row_item, parent, false)
@@ -25,28 +20,16 @@ class AnswersRecyclerAdapter(private val answers: List<Answer>, private val list
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         holder.questionButton.text = answers.elementAt(position).answer
         holder.questionButton.setOnClickListener {
-            val isCorrect = answers.elementAt(position).correct
-            updateTintColor(holder, isCorrect)
-            if (!isCorrect){
-                for (i in answers.indices) {
-                    if (answers[i].correct) {
-                        notifyItemChanged(i, "markCorrect")
-                        break
-                    }
-                }
-            }
-            listener.onCorrect(isCorrect)
+            handleButtonClicked(position, holder)
         }
     }
 
-    override fun onBindViewHolder(holder: ViewHolder, position: Int, payloads: MutableList<Any>) {
-        super.onBindViewHolder(holder, position, payloads)
-        for (payload in payloads){
-            if (payload is String && (payload as String).equals("markCorrect")) {
-                updateTintColor(holder, true)
-                break
-            }
-        }
+    private fun handleButtonClicked(position: Int, holder: ViewHolder) {
+        val isCorrect = answers.elementAt(position).correct
+        updateTintColor(holder, isCorrect)
+        if (!isCorrect)
+            updateCorrectQuestion()
+        listener.onCorrect(isCorrect)
     }
 
     private fun updateTintColor(holder: ViewHolder, isCorrect: Boolean) {
@@ -55,11 +38,38 @@ class AnswersRecyclerAdapter(private val answers: List<Answer>, private val list
         holder.questionButton.backgroundTintList = context.resources.getColorStateList(color, null)
     }
 
+    private fun updateCorrectQuestion() {
+        for (i in answers.indices) {
+            if (answers[i].correct) {
+                notifyItemChanged(i, HIGHLIGHT_CORRECT_ANSWER)
+                break
+            }
+        }
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int, payloads: MutableList<Any>) {
+        super.onBindViewHolder(holder, position, payloads)
+        for (payload in payloads){
+            if (payload is String && payload == HIGHLIGHT_CORRECT_ANSWER) {
+                updateTintColor(holder, true)
+                break
+            }
+        }
+    }
+
     override fun getItemCount(): Int {
         return answers.size
     }
+
+    class ViewHolder (view: View) : RecyclerView.ViewHolder(view) {
+        val questionButton: Button
+
+        init {
+            questionButton = view.findViewById(R.id.button_question)
+        }
+    }
 }
 
-public interface OnCorrectAnswerListener{
+interface OnCorrectAnswerListener{
     fun onCorrect(isCorrect: Boolean)
 }
