7030c0b634f46af9b484cea0b4623216b891ec8a
Edgar <Edgar@MacBook-Pro.fritz.box>
2022-10-15 22:31:36 +0200

    MainActivity refactoring

--- app/src/main/java/com/dosparta/triviagame/MainActivity.kt
+++ app/src/main/java/com/dosparta/triviagame/MainActivity.kt
@@ -1,47 +1,46 @@
 package com.dosparta.triviagame
 
-import android.content.Context
 import android.content.Intent
-import android.net.ConnectivityManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.text.Html
+import android.util.Log
 import android.view.View
 import android.widget.ProgressBar
 import android.widget.TextView
 import androidx.appcompat.app.AlertDialog
-import androidx.cardview.widget.CardView
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.android.volley.Request
-import com.android.volley.Response
 import com.android.volley.toolbox.StringRequest
-import com.android.volley.toolbox.Volley
 import com.google.android.material.snackbar.Snackbar
 import kotlinx.serialization.decodeFromString
 import kotlinx.serialization.json.Json
-import java.net.InetAddress
-import java.net.URLDecoder.decode
-import java.net.URLEncoder
 
 class MainActivity : AppCompatActivity(), OnCorrectAnswerListener {
 
-    val questions: MutableList<Question> = mutableListOf()
-    var parentLayout: ConstraintLayout? = null
-    var questionTitleTv: TextView? = null
-    var questionTv: TextView? = null
-    var loadingBar: ProgressBar? = null
-    var answersRecyclerView: RecyclerView? = null
-    var answersRecyclerAdapter: AnswersRecyclerAdapter? = null
-    var currentQuestion: Int = 0
-    var correctAnswers: Int = 0
+    private val tag = MainActivity::class.java.simpleName
+
+    private val questions: MutableList<Question> = mutableListOf()
+    private var parentLayout: ConstraintLayout? = null
+    private var questionTitleTv: TextView? = null
+    private var questionTv: TextView? = null
+    private var loadingBar: ProgressBar? = null
+    private var answersRecyclerView: RecyclerView? = null
+    private var answersRecyclerAdapter: AnswersRecyclerAdapter? = null
+    private var currentQuestion: Int = 0
+    private var correctAnswers: Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        attachUI()
+    }
+
+    private fun attachUI() {
         parentLayout = findViewById(R.id.parent_layout)
         questionTitleTv = findViewById(R.id.text_view_out_of)
         questionTv = findViewById(R.id.question_textview)
@@ -52,48 +51,59 @@ class MainActivity : AppCompatActivity(), OnCorrectAnswerListener {
 
     override fun onStart() {
         super.onStart()
-        loadingBar?.visibility = View.VISIBLE
-        parentLayout?.alpha = 0.5f
+        loadQuestions()
+    }
 
-        val url = "https://opentdb.com/api.php?amount=10"
-        val stringRequest = StringRequest(Request.Method.GET, url, { response ->
-            println("Response is: ${response.substring(0, 500)}")
-            //todo -> fill Questions
-            val jsonData = Json.decodeFromString<QuestionsSchema>(response)
-            questions.addAll(questionsSchemaToQuestions(jsonData))
-            questionTitleTv?.text = getString(R.string.out_of_text_placeholders, currentQuestion + 1, questions.size)
-            questionTv?.text = questions[currentQuestion].question
-            println(jsonData)
-            // todo --> present question on the screen
-
-            initAdapter(currentQuestion)
-            loadingBar?.visibility = View.GONE
-            parentLayout?.alpha = 1f
+    private fun loadQuestions() {
+        setLoadingState(true)
+        val stringRequest = StringRequest(Request.Method.GET, Utils.TRIVIA_API_URL, { response ->
+            onQuestionsReceived(response)
         }, {
-            println("Unable to retrieve data: ${it.networkResponse}")
-            val builder = AlertDialog.Builder(this)
-            val statusCode = if (it.networkResponse != null) it.networkResponse.statusCode else ""
-            builder.setTitle("Network error! $statusCode")
-                .setMessage("A network error ocurred." +
-                        "\nDo you want to retry or close the App")
-                .setPositiveButton("Retry") {dialog, _ ->
-                    dialog.dismiss()
-                    startActivity(Intent(this, MainActivity::class.java))
-                    finish()
-                }
-                .setNegativeButton("Close App"){dialog, _ ->
-                    dialog.dismiss()
-                    finishAffinity()
-                }
-                .setCancelable(false)
-                .show()
+            Log.i(tag, "Unable to retrieve data: ${it.networkResponse}")
+            showErrorDialog(if (it.networkResponse != null) it.networkResponse.statusCode else Utils.INTERNAL_SERVER_ERROR)
         })
         VolleySingleton.getInstance(this).addToRequestQueue(stringRequest)
     }
 
-    private fun initAdapter(questionPos: Int) {
-        answersRecyclerAdapter = AnswersRecyclerAdapter(questions[questionPos].answers, this)
-        answersRecyclerView?.adapter = answersRecyclerAdapter
+    private fun setLoadingState(loading: Boolean) {
+        loadingBar?.visibility = if (loading) View.VISIBLE else View.GONE
+        parentLayout?.alpha = if (loading) 0.5f else 1.0f
+    }
+
+    private fun onQuestionsReceived(response: String) {
+        Log.i(tag, "Response (first 500 chars): ${response.substring(0, 500)}")
+        fillQuestionList(response)
+        presentQuestion()
+        setLoadingState(false)
+    }
+
+    private fun showErrorDialog(statusCode: Int) {
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle(getString(R.string.network_error_popup_title, statusCode))
+            .setMessage(R.string.network_error_popup_msg)
+            .setPositiveButton(R.string.network_error_popup_positive_msg) { dialog, _ ->
+                dialog.dismiss()
+                startActivity(Intent(this, MainActivity::class.java))
+                finish()
+            }
+            .setNegativeButton(R.string.network_error_popup_negative_msg) { dialog, _ ->
+                dialog.dismiss()
+                finishAffinity()
+            }
+            .setCancelable(false)
+            .show()
+    }
+
+    private fun fillQuestionList(response: String) {
+        val jsonData = Json.decodeFromString<QuestionsSchema>(response)
+        questions.addAll(questionsSchemaToQuestions(jsonData))
+        Log.i(tag, "onStart: $jsonData")
+    }
+
+    private fun presentQuestion() {
+        questionTitleTv?.text = getString(R.string.out_of_text_placeholders, currentQuestion + 1, questions.size)
+        questionTv?.text = questions[currentQuestion].question
+        initAdapter(currentQuestion)
     }
 
     private fun questionsSchemaToQuestions(questionsSchema: QuestionsSchema) : List<Question>
@@ -115,46 +125,52 @@ class MainActivity : AppCompatActivity(), OnCorrectAnswerListener {
 
             questionList.add(question)
         }
-        println("list of question $questionList")
+        Log.i(tag, "list of question $questionList")
         return questionList
     }
 
+    private fun initAdapter(questionPos: Int) {
+        answersRecyclerAdapter = AnswersRecyclerAdapter(questions[questionPos].answers, this)
+        answersRecyclerView?.adapter = answersRecyclerAdapter
+    }
+
     override fun onCorrect(isCorrect: Boolean) {
-        Snackbar.make(window.decorView.rootView, "Your answers was $isCorrect", Snackbar.LENGTH_SHORT).show()
+        Snackbar.make(window.decorView.rootView, getString(R.string.right_question_overlay, isCorrect), Snackbar.LENGTH_SHORT).show()
         if (isCorrect){
             ++correctAnswers
         }
+        moveToNextQuestionWithDelay()
+    }
+
+    private fun moveToNextQuestionWithDelay() {
         val handler = Handler(mainLooper)
         handler.postDelayed({
             moveToNextQuestion()
-        }, 3000)
+        }, JUMP_TO_NEXT_QUESTION_DELAY)
     }
 
     private fun moveToNextQuestion(){
         if (currentQuestion == (questions.size - 1)){
-            Snackbar.make(window.decorView.rootView, "Game is over. Correct answer $correctAnswers/${questions.size}", Snackbar.LENGTH_SHORT).show()
-            //todo show dialog with restart game with same questions or with new questions
+            Snackbar.make(window.decorView.rootView, getString(R.string.game_over_overlay, correctAnswers, questions.size)
+                , Snackbar.LENGTH_SHORT).show()
             showResults()
             return
         }
-        currentQuestion++
-        questionTv?.text = questions[currentQuestion].question
-        questionTitleTv?.text = getString(R.string.out_of_text_placeholders, currentQuestion + 1, questions.size)
-        initAdapter(currentQuestion)
+        ++currentQuestion
+        presentQuestion()
     }
 
     private fun showResults() {
         val builder = AlertDialog.Builder(this)
-        builder.setTitle("Game over!")
-            .setMessage("Correct answers $correctAnswers/${questions.size}" +
-                    "\nScore ${(correctAnswers * 100) / questions.size}%" +
-                    "\nWould you like to start a new game or repeat this game (same questions)?")
-            .setPositiveButton("New") {dialog, _ ->
+        val correctAnswersOverTotal = (correctAnswers * 100) / questions.size
+        builder.setTitle(R.string.game_over_popup_title)
+            .setMessage(getString(R.string.game_over_popup_msg, correctAnswers, questions.size, correctAnswersOverTotal))
+            .setPositiveButton(R.string.game_over_popup_positive_msg) {dialog, _ ->
                 dialog.dismiss()
                 startActivity(Intent(this, MainActivity::class.java))
                 finish()
             }
-            .setNegativeButton("Repeat"){dialog, _ ->
+            .setNegativeButton(R.string.game_over_popup_negative_msg){dialog, _ ->
                 dialog.dismiss()
                 resetGame()
             }
@@ -168,5 +184,9 @@ class MainActivity : AppCompatActivity(), OnCorrectAnswerListener {
         questionTitleTv?.text = getString(R.string.out_of_text_placeholders, currentQuestion + 1, questions.size)
         questionTv?.text = questions[currentQuestion].question
         initAdapter(currentQuestion)
+    }
+
+    companion object {
+        private const val JUMP_TO_NEXT_QUESTION_DELAY = 3000L
     }
 }
---
+++ app/src/main/java/com/dosparta/triviagame/Utils.kt
@@ -0,0 +1,8 @@
+package com.dosparta.triviagame
+
+class Utils {
+    companion object {
+        const val TRIVIA_API_URL = "https://opentdb.com/api.php?amount=10"
+        const val INTERNAL_SERVER_ERROR = 500
+    }
+}
