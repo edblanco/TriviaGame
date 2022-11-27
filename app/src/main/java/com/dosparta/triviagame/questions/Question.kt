c5867d40b56035e023fcfa3474546141e6b9a481
Edgar <Edgar@MacBook-Pro.fritz.box>
2022-11-27 19:19:55 +0100

    Changing min SDK and allowing rotation

--- app/src/main/java/com/dosparta/triviagame/questions/Question.kt
+++ app/src/main/java/com/dosparta/triviagame/questions/Question.kt
@@ -1,14 +1,19 @@
 package com.dosparta.triviagame.questions
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
 data class Question(
     val category: String,
     val difficulty: String,
     val answers: List<Answer>,
     val question: String,
     val type: String
-)
+): Parcelable
 
+@Parcelize
 data class Answer (
     val answer: String,
     val correct: Boolean
-    )
+    ): Parcelable
