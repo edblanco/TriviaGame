a36c8fbf680f3a7dae6c6aa3f20c762ec025c000
Edgar <Edgar@MacBook-Pro-2.fritz.box>
2023-03-12 15:12:22 +0100

    Extracting Volley interface

--- app/src/main/java/com/dosparta/triviagame/common/dependencyinjection/CompositionRoot.kt
+++ app/src/main/java/com/dosparta/triviagame/common/dependencyinjection/CompositionRoot.kt
@@ -1,19 +1,20 @@
 package com.dosparta.triviagame.common.dependencyinjection
 
 import android.content.Context
+import com.dosparta.triviagame.networking.IVolleySingleton
 import com.dosparta.triviagame.networking.VolleySingleton
 import com.dosparta.triviagame.screens.common.dialogs.DialogsEventBus
 
 class CompositionRoot {
 
-    private var volley: VolleySingleton? = null
+    private var volley: IVolleySingleton? = null
     private var dialogsEventBus: DialogsEventBus? = null
 
-    fun getVolleyInstance(context: Context): VolleySingleton {
+    fun getVolleyInstance(context: Context): IVolleySingleton {
         return getVolley(context.applicationContext)
     }
 
-    private fun getVolley(context: Context): VolleySingleton {
+    private fun getVolley(context: Context): IVolleySingleton {
         volley ?: VolleySingleton(context.applicationContext).also {
             volley = it
         }
