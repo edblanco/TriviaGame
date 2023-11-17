deaf3c3b58c8624027e7a55104c46eccae591ed5
Edgar <Edgar@MacBook-Pro-2.fritz.box>
2023-11-17 23:38:28 +0100

    Style(Theme): Updating app theme

--- app/src/main/java/com/dosparta/triviagame/screens/trivia/answersitem/IUIAnswersItemConfig.kt
+++ app/src/main/java/com/dosparta/triviagame/screens/trivia/answersitem/IUIAnswersItemConfig.kt
@@ -1,5 +1,5 @@
 package com.dosparta.triviagame.screens.trivia.answersitem
 
 interface IUIAnswersItemConfig {
-    fun getAnswerTintColor(isCorrect: Boolean): Int
+    fun getAnswerTintColor(isCorrect: Boolean, isDarkModeActive: Boolean): Int
 }
--- app/src/main/java/com/dosparta/triviagame/screens/trivia/answersitem/UIAnswersItemConfig.kt
+++ app/src/main/java/com/dosparta/triviagame/screens/trivia/answersitem/UIAnswersItemConfig.kt
@@ -3,5 +3,11 @@ package com.dosparta.triviagame.screens.trivia.answersitem
 import com.dosparta.triviagame.R
 
 class UIAnswersItemConfig : IUIAnswersItemConfig {
-    override fun getAnswerTintColor(isCorrect: Boolean) = if (isCorrect) R.color.hunter_green else R.color.blood_red
+    override fun getAnswerTintColor(isCorrect: Boolean, isDarkModeActive: Boolean): Int {
+        return if (isDarkModeActive){
+            if (isCorrect) R.color.md_theme_dark_tertiary else R.color.md_theme_dark_error
+        } else  {
+            if (isCorrect) R.color.md_theme_light_tertiary else R.color.md_theme_light_error
+        }
+    }
 }
